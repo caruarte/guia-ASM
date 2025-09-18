26c3ce42dbfecf4cf34ffc03c207bd21c58d6cbc
caruarte <caruarte@gmail.com>
2025-09-18 11:44:45 -0300

    Un poco del 3

--- src/7 - Ejercicios integradores/ej3/ej3a/ej3a.c
+++ src/7 - Ejercicios integradores/ej3/ej3a/ej3a.c
@@ -1,11 +1,69 @@
 #include "../ejs.h"
 
 // Función auxiliar para contar casos por nivel
-void contar_casos_por_nivel(caso_t* arreglo_casos, int largo, int* contadores) {
+uint32_t contar_casos_por_nivel(caso_t* arreglo_casos, uint32_t largo, uint32_t nivel) {
+    uint32_t res = 0;
+    for (int i = 0; i < largo; i++){
+        if ((arreglo_casos[i].usuario) -> nivel == nivel){
+            res++;
+        }
+    }
+    return res;
 }
 
 
 segmentacion_t* segmentar_casos(caso_t* arreglo_casos, int largo) {
+
+    uint32_t cant0 = contar_casos_por_nivel(arreglo_casos, largo, 0);
+    uint32_t cant1 = contar_casos_por_nivel(arreglo_casos, largo, 1);
+    uint32_t cant2 = contar_casos_por_nivel(arreglo_casos, largo, 2);
+
+
+
+    caso_t* casos0;
+    caso_t* casos1;
+    caso_t* casos2;
+
+    if (cant0 > 0){
+        casos0 = malloc(cant0*sizeof(caso_t));
+    } else {
+        casos0 = NULL;
+    }
+    if (cant1 > 0){
+        casos1 = malloc(cant1*sizeof(caso_t));
+    } else {
+        casos1 = NULL;
+    }
+    if (cant2 > 0){
+        casos2 = malloc(cant2*sizeof(caso_t));
+    } else {
+        casos2 = NULL;
+    }
+    
+
+    int indice0 = 0;
+    int indice1 = 0;
+    int indice2 = 0;
+
+    for (int i = 0; i < largo; i++){
+        uint32_t nivelCaso = (arreglo_casos[i].usuario) -> nivel;
+        if (nivelCaso == 0){
+            casos0[indice0] = arreglo_casos[i];
+            indice0++;
+        } else if (nivelCaso == 1){
+            casos1[indice1] = arreglo_casos[i];
+            indice1++;
+        } else{
+            casos2[indice2] = arreglo_casos[i];
+            indice2++;
+        }
+    }
+
+    segmentacion_t* res = malloc(24);
+    res -> casos_nivel_0 = casos0;
+    res -> casos_nivel_1 = casos1;
+    res -> casos_nivel_2 = casos2;
+    return res;
 }
 
 
--- src/7 - Ejercicios integradores/ej3/ej3c/ej3c.c
+++ src/7 - Ejercicios integradores/ej3/ej3c/ej3c.c
@@ -1,5 +1,19 @@
 #include "../ejs.h"
 
 estadisticas_t* calcular_estadisticas(caso_t* arreglo_casos, int largo, uint32_t usuario_id){
+
+    estadisticas_t* res = malloc(7);
+
+    for (int i = 0; i < largo; i++){
+        if (usuario_id == 0 || usuario_id == arreglo_casos[i].usuario -> id){
+            char* categoria = arreglo_casos[i].categoria;
+            uint16_t estado = arreglo_casos[i].estado;
+
+            if (categoria == "CLT"){
+
+            }
+        }
+    }
+    
 }
 
